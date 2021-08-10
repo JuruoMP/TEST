d396c7e328cccfbdaf53ea5cd5f0552fc363c90c
juruo <815732072@qq.com>
2021-08-10 21:56:14 +0800

    add text2sql

---
+++ run_lightning_text2sql.py
@@ -0,0 +1,34 @@
+import os
+import sys
+sys.path.append(os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
+
+from torch.utils.data import DataLoader
+import pytorch_lightning as pl
+from pytorch_lightning.callbacks.early_stopping import EarlyStopping
+
+from model import SQLBart
+from sparc_dataset import SparcDataModule
+from transformers import BartTokenizer
+
+
+if __name__ == '__main__':
+    config_name = 'facebook/bart-large'
+    bart_tokenizer = BartTokenizer.from_pretrained(config_name)
+    sql_bart = SQLBart(bart_tokenizer)
+
+    sparc_data = SparcDataModule('sparc/', batch_size=32, tokenizer=bart_tokenizer)
+    trainer = pl.Trainer(gpus=-1, precision=16, default_root_dir='checkpoints',
+                         terminate_on_nan=True, accumulate_grad_batches=1,
+                         # check_val_every_n_epoch=20,
+                         gradient_clip_val=5,
+                         callbacks=[EarlyStopping(monitor='val_loss', patience=10, mode='min')],
+                         )
+    # trainer = pl.Trainer(default_root_dir='checkpoints',
+    #                      terminate_on_nan=True, accumulate_grad_batches=1,
+    #                      check_val_every_n_epoch=5,
+    #                      gradient_clip_val=5, gradient_clip_algorithm='value',
+    #                      callbacks=[EarlyStopping(monitor='val_loss', patience=10, mode='min')],
+    #                      )
+    trainer.fit(model=sql_bart, datamodule=sparc_data)
+
+    trainer.test(model=sql_bart, datamodule=sparc_data)
